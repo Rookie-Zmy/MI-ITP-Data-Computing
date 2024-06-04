43803292f585513f636a0f38d6772af9fad8841e
molin24 <2659173131@qq.com>
2024-06-04 19:58:28 +0800

    Update 5.31.java

--- zls-code/5.31.java
+++ zls-code/5.31.java
@@ -1,4 +1,6 @@
 class Solution {
+    //找出缺失和重复的数字  给你一个下标从 0 开始的二维整数矩阵 grid，大小为 n * n ，其中的值在 [1, n2] 范围内。除了 a 出现 两次，b 缺失 之外，每个整数都 恰好出现一次 。
+任务是找出重复的数字a 和缺失的数字 b 。返回一个下标从 0 开始、长度为 2 的整数数组 ans ，其中 ans[0] 等于 a ，ans[1] 等于 b 。
     public int[] findMissingAndRepeatedValues(int[][] grid) {
         int n=grid.length;
         int[] k=new int[n*n+1];
