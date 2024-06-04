7f0bcda5f53b0088084ff28d6f1147808e220d1b
molin24 <2659173131@qq.com>
2024-06-04 19:53:23 +0800

    Update 5.28.java

--- zls-code/5.28.java
+++ zls-code/5.28.java
@@ -1,4 +1,9 @@
 class Solution {
+    //给你一个下标从 0 开始的数组 mountain 。你的任务是找出数组 mountain 中的所有 峰值。
+     以数组形式返回给定数组中 峰值 的下标，顺序不限 。
+     注意：
+     峰值 是指一个严格大于其相邻元素的元素。
+     数组的第一个和最后一个元素 不 是峰值。
     public List<Integer> findPeaks(int[] mountain) {
         List<Integer> ans = new ArrayList<>();
         int n = mountain.length;
