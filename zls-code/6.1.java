b02c979bfbc5a10bd6e2511874a8744a7bd17836
molin24 <2659173131@qq.com>
2024-06-10 15:56:23 +0800

    Update 6.1.java

--- zls-code/6.1.java
+++ zls-code/6.1.java
@@ -1,8 +1,4 @@
 class Solution {
-<<<<<<< HEAD
-=======
-    //给小朋友们分糖果 给你两个正整数 n 和 limit 。请你将 n 颗糖果分给 3 位小朋友，确保没有任何小朋友得到超过 limit 颗糖果，请你返回满足此条件下的总方案数 。
->>>>>>> 8a22dddc8517e9bbe4916cc391991ad5b2fa012f
     public int distributeCandies(int n, int limit) {
         int count=0;
         int[] a=new int[3];
