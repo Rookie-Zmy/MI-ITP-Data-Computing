ef2ff789021a6c0e55b8a636bfd3fe0c80e49489
Kukdo <1019957525@qq.com>
2024-06-14 20:45:54 +0800

    Upload springboot demo project

---
+++ jzp-code/spring_test/src/main/java/org/example/MyApplication.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class MyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+
+    @RestController
+    public static class HelloController {
+
+        @GetMapping("/")
+        public String hello() {
+            return "Hello, World!";
+        }
+    }
+}
