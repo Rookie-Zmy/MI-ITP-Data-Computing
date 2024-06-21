0ae746b2eb72800e130033fd5cef8814fd7aba16
Kukdo <1019957525@qq.com>
2024-06-21 14:31:20 +0800

    Upload practice projects with mentor's suggestion

---
+++ jzp-code/practice-projects/boot-demo/src/test/java/com/tutorial/bootDemo/BootDemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.tutorial.bootDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
